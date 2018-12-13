9705be8f5e2e6bc6b23f1681a1cabcf60024f991
Brennan Forgues <bforgues@Brennans-MacBook-Pro.local>
2018-12-13 12:24:32 -0500

    return jwt from create user resolver

--- resolvers/Mutation.ts
+++ resolvers/Mutation.ts
@@ -1,11 +1,21 @@
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+const APP_SECRET = "GraphQL-is-aw3some"
 
 const createUser = async (root, args, context) => {
   const hashedPassword = await bcrypt.hash(args.password, 10)
-  return await context.prisma.createUser({
+  const user = await context.prisma.createUser({
     username: args.username,
     hashedPassword: hashedPassword
   })
+  
+  const token = jwt.sign({ userId: user.id }, APP_SECRET)
+  
+  return {
+    user, 
+    token,
+  }
 }
 
 module.exports = {
---
+++ resolvers/utils.ts
@@ -0,0 +1,18 @@
+
+
+
+// const getUserId = (context) => {
+//   const authorization = context.request.get("Authorization")
+//   if (authorization) {
+//     const token = authorization.replace("Bearer ", "")
+//     const { userId } = jwt.verify(token, APP_SECRET)
+//     return userId
+//   }
+
+//   throw new Error('Not authenticated')
+// }
+
+// module.exports = {
+//   APP_SECRET,
+//   getUserId,
+// }
